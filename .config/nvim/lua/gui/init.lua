ac0ec81537d23858f07e7d59c4293c92ec984109
tp86 <tom0palka+tp86@gmail.com>
2022-10-07 14:19:42 +0200

    Add mouse support

--- .config/nvim/lua/gui/init.lua
+++ .config/nvim/lua/gui/init.lua
@@ -13,5 +13,6 @@ api.nvim_create_autocmd({ "UIEnter" }, {
       cmd 'GuiFont! Hack:h11'
       opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
     end
+    opt.mouse = 'a'
   end,
 })
