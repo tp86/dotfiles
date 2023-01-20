933f838fcb4b92f7c5eca3854f19d2ea57c5389e
tp86 <tom0palka+tp86@gmail.com>
2023-01-20 14:05:13 +0100

    neovim gui setup fix

--- .config/nvim/lua/gui/init.lua
+++ .config/nvim/lua/gui/init.lua
@@ -1,18 +1,22 @@
 local api = vim.api
 local cmd = vim.cmd
+local fn = vim.fn
 local opt = vim.opt
-local v = vim.v
+
+local function execcmdifexists(command)
+  if fn.exists(":" .. string.match(command[1], "%w+")) ~= 0 then
+    cmd(table.concat(command, " "))
+  end
+end
 
 local group = api.nvim_create_augroup('GuiConfig', { clear = true })
 api.nvim_create_autocmd({ "UIEnter" }, {
   group = group,
   callback = function()
-    if v.event.chan == 1 then
-      cmd 'GuiTabline 0'
-      cmd 'GuiPopupmenu 0'
-      cmd 'GuiFont! Hack:h11'
-      opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
-    end
+    execcmdifexists { "GuiTabline", 0 }
+    execcmdifexists { "GuiPopupmenu", 0 }
+    execcmdifexists { "GuiFont!", "Hack:h11" }
+    opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
     opt.mouse = 'a'
   end,
 })
